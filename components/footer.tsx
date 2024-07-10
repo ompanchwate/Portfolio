fc93542a1f078d96a63e9d5dbe9d7428e74c8520
ompanchwate2003 <ompanchwate2003@gmail.com>
2024-07-10 10:26:15 +0530

    added buyMeACoffee

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
       <p>
         <span className="text-md font-bold tracking-wide mt-3">Made with ❤️ by Om Panchwate</span>
       </p>
-        
+      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="ompanchwatv" data-description="Support me on Buy me a coffee!" data-message="" data-color="#5F7FFF" data-position="right" data-x_margin="18" data-y_margin="18"></script>
     </footer>
   );
 }
